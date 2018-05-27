5833f9e5577cd7f0a5de94c6327d5f7a49eb355b
Alex <alexander.krolik@mail.mcgill.ca>
2018-05-27 17:05:12 -0400

    Use alias types in contorl flow test.

--- tests/PTX/ControlFlowTest.h
+++ tests/PTX/ControlFlowTest.h
@@ -8,6 +8,10 @@
 #include "PTX/Block.h"
 #include "PTX/Functions/EntryFunction.h"
 
+#include "PTX/StateSpaces/AddressableSpace.h"
+#include "PTX/StateSpaces/RegisterSpace.h"
+#include "PTX/Operands/Variables/AddressableVariable.h"
+
 #include "PTX/Instructions/ControlFlow/BranchInstruction.h"
 #include "PTX/Instructions/ControlFlow/BranchIndexInstruction.h"
 #include "PTX/Instructions/ControlFlow/CallInstruction.h"
@@ -28,9 +32,9 @@ public:
 
 		PTX::RegisterSpace<PTX::Bit32Type> *param_b32 = new PTX::RegisterSpace<PTX::Bit32Type>("%b");
 		PTX::RegisterSpace<PTX::Int32Type> *param_s32 = new PTX::RegisterSpace<PTX::Int32Type>("%s");
-		PTX::AddressableSpace<PTX::UInt64Type, PTX::AddressSpace::Param> *param_u64 = new PTX::AddressableSpace<PTX::UInt64Type, PTX::AddressSpace::Param>("%u");
+		PTX::ParameterSpace<PTX::UInt64Type> *param_u64 = new PTX::ParameterSpace<PTX::UInt64Type>("%u");
 
-		auto deviceFunction = new PTX::DataFunction<PTX::RegisterSpace<PTX::Bit32Type>, PTX::RegisterSpace<PTX::Int32Type>, PTX::AddressableSpace<PTX::UInt64Type, PTX::AddressSpace::Param>>();
+		auto deviceFunction = new PTX::DataFunction<PTX::RegisterSpace<PTX::Bit32Type>, PTX::RegisterSpace<PTX::Int32Type>, PTX::ParameterSpace<PTX::UInt64Type>>();
 		deviceFunction->SetName("device_function");
 		deviceFunction->SetReturnSpace(param_b32);
 		deviceFunction->SetParameters(param_s32, param_u64);
@@ -45,7 +49,7 @@ public:
 
 		PTX::RegisterSpace<PTX::Bit32Type> *b32 = new PTX::RegisterSpace<PTX::Bit32Type>("%b");
 		PTX::RegisterSpace<PTX::Int32Type> *s32 = new PTX::RegisterSpace<PTX::Int32Type>("%s");
-		PTX::AddressableSpace<PTX::UInt64Type, PTX::AddressSpace::Param> *u64 = new PTX::AddressableSpace<PTX::UInt64Type, PTX::AddressSpace::Param>("%u");
+		PTX::ParameterSpace<PTX::UInt64Type> *u64 = new PTX::ParameterSpace<PTX::UInt64Type>("%u");
 
 		block->AddStatement(b32);
 		block->AddStatement(s32);
@@ -53,9 +57,9 @@ public:
 
 		PTX::Register<PTX::Bit32Type> *regb32 = b32->GetVariable("%b");
 		PTX::Register<PTX::Int32Type> *regs32 = s32->GetVariable("%s");
-		PTX::Variable<PTX::UInt64Type, PTX::AddressableSpace<PTX::UInt64Type, PTX::AddressSpace::Param>> *varu64 = u64->GetVariable("%u");
+		PTX::ParameterVariable<PTX::UInt64Type> *varu64 = u64->GetVariable("%u");
 
-		block->AddStatement(new PTX::CallInstruction<PTX::RegisterSpace<PTX::Bit32Type>, PTX::RegisterSpace<PTX::Int32Type>, PTX::AddressableSpace<PTX::UInt64Type, PTX::AddressSpace::Param>>(deviceFunction, regb32, regs32, varu64));
+		block->AddStatement(new PTX::CallInstruction<PTX::RegisterSpace<PTX::Bit32Type>, PTX::RegisterSpace<PTX::Int32Type>, PTX::ParameterSpace<PTX::UInt64Type>>(deviceFunction, regb32, regs32, varu64));
 
 		entryFunction->SetBody(block);
 		
