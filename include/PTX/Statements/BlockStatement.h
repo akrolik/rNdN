4daac06a538ae99728f7360b3665c79aa31b9b45
Alex <alexander.krolik@mail.mcgill.ca>
2018-06-19 21:36:49 -0400

    Add convenience methods for inserting statements into block.

--- include/PTX/Statements/BlockStatement.h
+++ include/PTX/Statements/BlockStatement.h
@@ -14,6 +14,21 @@ public:
 	{
 		m_statements.push_back(statement);
 	}
+	template<class T>
+	void AddStatements(const std::vector<T>& statements)
+	{
+		m_statements.insert(std::end(m_statements), std::begin(statements), std::end(statements));
+	}
+
+	void InsertStatement(const Statement *statement, unsigned int index)
+	{
+		m_statements.insert(std::begin(m_statements) + index, statement);
+	}
+	template<class T>
+	void InsertStatements(const std::vector<T>& statements, unsigned int index)
+	{
+		m_statements.insert(std::begin(m_statements) + index, std::begin(statements), std::end(statements));
+	}
 
 	std::string ToString() const override
 	{
