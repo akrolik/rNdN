0acc59276c0d239efb34a372034481f61d656236
Alex <alexander.krolik@mail.mcgill.ca>
2018-06-23 16:33:42 -0400

    Generalize adapter.

--- include/PTX/Operands/Adapters/Adapter.h
+++ include/PTX/Operands/Adapters/Adapter.h
@@ -7,17 +7,17 @@ namespace PTX {
 template<template<Bits> class T, Bits B>
 class BitAdapter;
 
-template<template<Bits> class D, template<Bits> class S, Bits B>
-class Adapter : public Operand<D<B>>
+template<template<Bits> class D, template<Bits> class S, Bits BD, Bits BS = BD>
+class Adapter : public Operand<D<BD>>
 {
-	friend class BitAdapter<S, B>;
+	friend class BitAdapter<S, BD>;
 public:
 	std::string ToString() const { return m_operand->ToString(); }
 
 protected:
-	Adapter(const Operand<S<B>> *operand) : m_operand(operand) {}
+	Adapter(const Operand<S<BS>> *operand) : m_operand(operand) {}
 
-	const Operand<S<B>> *m_operand = nullptr;
+	const Operand<S<BS>> *m_operand = nullptr;
 };
 
 }
