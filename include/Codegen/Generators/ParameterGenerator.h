7bfdd74c152b3a33b023d0a9f11ebbd0e8e16426
Alex <alexander.krolik@mail.mcgill.ca>
2018-07-09 16:55:38 -0400

    Add boolean parameters and return.

--- include/Codegen/Generators/ParameterGenerator.h
+++ include/Codegen/Generators/ParameterGenerator.h
@@ -9,6 +9,8 @@
 #include "PTX/Type.h"
 #include "PTX/Declarations/VariableDeclaration.h"
 #include "PTX/Functions/DataFunction.h"
+#include "PTX/Instructions/Comparison/SetPredicateInstruction.h"
+#include "PTX/Instructions/Data/ConvertInstruction.h"
 #include "PTX/Instructions/Data/LoadInstruction.h"
 #include "PTX/Statements/BlockStatement.h"
 
@@ -23,22 +25,43 @@ public:
 	template<class T>
 	static void Generate(HorseIR::Parameter *parameter, Builder *builder)
 	{
-		//TODO: Remove check for loading parameters
-		if constexpr(!std::is_same<T, PTX::PredicateType>::value)
+		//TODO: Specialize?
+		if constexpr(std::is_same<T, PTX::PredicateType>::value)
 		{
-		auto declaration = new PTX::PointerDeclaration<B, T>(parameter->GetName());
-		builder->AddParameter(declaration);
-		auto variable = declaration->GetVariable(parameter->GetName());
-		auto value = builder->AllocateRegister<T>(parameter->GetName());
+			auto declaration = new PTX::PointerDeclaration<B, PTX::Int8Type>(parameter->GetName());
+			builder->AddParameter(declaration);
+			auto variable = declaration->GetVariable(parameter->GetName());
+			auto value = builder->AllocateRegister<T>(parameter->GetName());
 
-		auto block = new PTX::BlockStatement();
-		builder->AddStatement(block);
-		builder->OpenScope(block);
+			auto block = new PTX::BlockStatement();
+			builder->AddStatement(block);
+			builder->OpenScope(block);
 
-		auto address = AddressGenerator<B>::template Generate<T>(variable, builder);
-		builder->AddStatement(new PTX::LoadInstruction<B, T, PTX::GlobalSpace>(value, address));
+			auto temp8 = builder->AllocateRegister<PTX::Int8Type, ResourceKind::Internal>(parameter->GetName());
+			auto address = AddressGenerator<B>::template Generate<PTX::Int8Type>(variable, builder);
+			builder->AddStatement(new PTX::LoadInstruction<B, PTX::Int8Type, PTX::GlobalSpace>(temp8, address));
 
-		builder->CloseScope();
+			auto temp16 = builder->AllocateRegister<PTX::Int16Type, ResourceKind::Internal>(parameter->GetName());
+			builder->AddStatement(new PTX::ConvertInstruction<PTX::Int16Type, PTX::Int8Type>(temp16, temp8));
+			builder->AddStatement(new PTX::SetPredicateInstruction<PTX::Int16Type>(value, temp16, new PTX::Value<PTX::Int16Type>(0), PTX::Int16Type::ComparisonOperator::NotEqual));
+
+			builder->CloseScope();
+		}
+		else
+		{
+			auto declaration = new PTX::PointerDeclaration<B, T>(parameter->GetName());
+			builder->AddParameter(declaration);
+			auto variable = declaration->GetVariable(parameter->GetName());
+			auto value = builder->AllocateRegister<T>(parameter->GetName());
+
+			auto block = new PTX::BlockStatement();
+			builder->AddStatement(block);
+			builder->OpenScope(block);
+
+			auto address = AddressGenerator<B>::template Generate<T>(variable, builder);
+			builder->AddStatement(new PTX::LoadInstruction<B, T, PTX::GlobalSpace>(value, address));
+
+			builder->CloseScope();
 		}
 	}
 };
--- include/Codegen/Generators/ReturnGenerator.h
+++ include/Codegen/Generators/ReturnGenerator.h
@@ -9,6 +9,8 @@
 #include "PTX/Type.h"
 #include "PTX/Declarations/VariableDeclaration.h"
 #include "PTX/Functions/DataFunction.h"
+#include "PTX/Instructions/Comparison/SelectInstruction.h"
+#include "PTX/Instructions/Data/ConvertInstruction.h"
 #include "PTX/Instructions/Data/StoreInstruction.h"
 #include "PTX/Instructions/ControlFlow/ReturnInstruction.h"
 #include "PTX/Statements/BlockStatement.h"
@@ -24,24 +26,48 @@ public:
 	template<class T>
 	static void Generate(HorseIR::ReturnStatement *ret, Builder *builder)
 	{
-		//TODO: remove check
-		if constexpr(!std::is_same<T, PTX::PredicateType>::value)
+		//TODO: Specialize?
+		if constexpr(std::is_same<T, PTX::PredicateType>::value)
 		{
-		const std::string returnName = "$return";
-		auto declaration = new PTX::PointerDeclaration<B, T>(returnName);
-		builder->AddParameter(declaration);
-		auto variable = declaration->GetVariable(returnName);
+			const std::string returnName = "$return";
+			auto declaration = new PTX::PointerDeclaration<B, PTX::Int8Type>(returnName);
+			builder->AddParameter(declaration);
+			auto variable = declaration->GetVariable(returnName);
 
-		auto block = new PTX::BlockStatement();
-		builder->AddStatement(block);
-		builder->OpenScope(block);
+			auto block = new PTX::BlockStatement();
+			builder->AddStatement(block);
+			builder->OpenScope(block);
 
-		auto address = AddressGenerator<B>::template Generate<T>(variable, builder);
-		auto value = builder->GetRegister<T>(ret->GetVariableName());
-		builder->AddStatement(new PTX::StoreInstruction<B, T, PTX::GlobalSpace>(address, value));
-		builder->AddStatement(new PTX::ReturnInstruction());
+			auto address = AddressGenerator<B>::template Generate<PTX::Int8Type>(variable, builder);
+			auto value = builder->GetRegister<PTX::PredicateType>(ret->GetVariableName());
 
-		builder->CloseScope();
+			auto temp32 = builder->AllocateRegister<PTX::Int32Type, ResourceKind::Internal>(ret->GetVariableName());
+			auto temp8 = builder->AllocateRegister<PTX::Int8Type, ResourceKind::Internal>(ret->GetVariableName());
+
+			builder->AddStatement(new PTX::SelectInstruction<PTX::Int32Type>(temp32, new PTX::Value<PTX::Int32Type>(1), new PTX::Value<PTX::Int32Type>(0), value));
+			builder->AddStatement(new PTX::ConvertInstruction<PTX::Int8Type, PTX::Int32Type>(temp8, temp32));
+			builder->AddStatement(new PTX::StoreInstruction<B, PTX::Int8Type, PTX::GlobalSpace>(address, temp8));
+			builder->AddStatement(new PTX::ReturnInstruction());
+
+			builder->CloseScope();
+		}
+		else
+		{
+			const std::string returnName = "$return";
+			auto declaration = new PTX::PointerDeclaration<B, T>(returnName);
+			builder->AddParameter(declaration);
+			auto variable = declaration->GetVariable(returnName);
+
+			auto block = new PTX::BlockStatement();
+			builder->AddStatement(block);
+			builder->OpenScope(block);
+
+			auto address = AddressGenerator<B>::template Generate<T>(variable, builder);
+			auto value = builder->GetRegister<T>(ret->GetVariableName());
+			builder->AddStatement(new PTX::StoreInstruction<B, T, PTX::GlobalSpace>(address, value));
+			builder->AddStatement(new PTX::ReturnInstruction());
+
+			builder->CloseScope();
 		}
 	}
 };
