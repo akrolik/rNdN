f493e97e0440e802bf565bcc662d5e72e5729b29
Alex <alexander.krolik@mail.mcgill.ca>
2018-07-27 21:33:50 -0400

    Update expression generator to use enum class for builtins.

--- include/Codegen/Generators/Expressions/ExpressionGenerator.h
+++ include/Codegen/Generators/Expressions/ExpressionGenerator.h
@@ -40,206 +40,112 @@ public:
 
 	BuiltinGenerator<B, T> *GetBuiltinGenerator(const HorseIR::CallExpression *call)
 	{
-		std::string name = call->GetName();
-		if (name == "@abs")
+		BuiltinFunction function = GetBuiltinFunction(call->GetName());
+		switch (function)
 		{
-			return new UnaryGenerator<B, T>(this->m_builder, UnaryOperation::Absolute);
+			case BuiltinFunction::Absolute:
+				return new UnaryGenerator<B, T>(this->m_builder, UnaryOperation::Absolute);
+			case BuiltinFunction::Negate:
+				return new UnaryGenerator<B, T>(this->m_builder, UnaryOperation::Negate);
+			case BuiltinFunction::Ceiling:
+				return new RoundingGenerator<B, T>(this->m_builder, RoundingOperation::Ceiling);
+			case BuiltinFunction::Floor:
+				return new RoundingGenerator<B, T>(this->m_builder, RoundingOperation::Floor);
+			case BuiltinFunction::Round:
+				return new RoundingGenerator<B, T>(this->m_builder, RoundingOperation::Nearest);
+			case BuiltinFunction::Conjugate:
+				//TODO: Add support for complex numbers
+				std::cerr << "[ERROR] Complex number functions are not supported" << std::endl;
+				std::exit(EXIT_FAILURE);
+			case BuiltinFunction::Reciprocal:
+				return new UnaryGenerator<B, T>(this->m_builder, UnaryOperation::Reciprocal);
+			case BuiltinFunction::Sign:
+				return new ComparisonGenerator<B, T>(this->m_builder, ComparisonOperator::Sign);
+			case BuiltinFunction::Pi:
+				return new UnaryGenerator<B, T>(this->m_builder, UnaryOperation::Pi);
+			case BuiltinFunction::Not:
+				return new UnaryGenerator<B, T>(this->m_builder, UnaryOperation::Not);
+			case BuiltinFunction::Logarithm:
+				return new ExternalUnaryGenerator<B, T>(this->m_builder, ExternalUnaryOperation::Logarithm);
+			case BuiltinFunction::Exponential:
+				return new ExternalUnaryGenerator<B, T>(this->m_builder, ExternalUnaryOperation::Exponential);
+			case BuiltinFunction::Cosine:
+				return new ExternalUnaryGenerator<B, T>(this->m_builder, ExternalUnaryOperation::Cosine);
+			case BuiltinFunction::Sine:
+				return new ExternalUnaryGenerator<B, T>(this->m_builder, ExternalUnaryOperation::Sine);
+			case BuiltinFunction::Tangent:
+				return new ExternalUnaryGenerator<B, T>(this->m_builder, ExternalUnaryOperation::Tangent);
+			case BuiltinFunction::InverseCosine:
+				return new ExternalUnaryGenerator<B, T>(this->m_builder, ExternalUnaryOperation::InverseCosine);
+			case BuiltinFunction::InverseSine:
+				return new ExternalUnaryGenerator<B, T>(this->m_builder, ExternalUnaryOperation::InverseSine);
+			case BuiltinFunction::InverseTangent:
+				return new ExternalUnaryGenerator<B, T>(this->m_builder, ExternalUnaryOperation::InverseTangent);
+			case BuiltinFunction::HyperbolicCosine:
+				return new ExternalUnaryGenerator<B, T>(this->m_builder, ExternalUnaryOperation::HyperbolicCosine);
+			case BuiltinFunction::HyperbolicSine:
+				return new ExternalUnaryGenerator<B, T>(this->m_builder, ExternalUnaryOperation::HyperbolicSine);
+			case BuiltinFunction::HyperbolicTangent:
+				return new ExternalUnaryGenerator<B, T>(this->m_builder, ExternalUnaryOperation::HyperbolicTangent);
+			case BuiltinFunction::HyperbolicInverseCosine:
+				return new ExternalUnaryGenerator<B, T>(this->m_builder, ExternalUnaryOperation::HyperbolicInverseCosine);
+			case BuiltinFunction::HyperbolicInverseSine:
+				return new ExternalUnaryGenerator<B, T>(this->m_builder, ExternalUnaryOperation::HyperbolicInverseSine);
+			case BuiltinFunction::HyperbolicInverseTangent:
+				return new ExternalUnaryGenerator<B, T>(this->m_builder, ExternalUnaryOperation::HyperbolicInverseTangent);
+			case BuiltinFunction::Less:
+				return new ComparisonGenerator<B, T>(this->m_builder, ComparisonOperator::Less);
+			case BuiltinFunction::Greater:
+				return new ComparisonGenerator<B, T>(this->m_builder, ComparisonOperator::Greater);
+			case BuiltinFunction::LessEqual:
+				return new ComparisonGenerator<B, T>(this->m_builder, ComparisonOperator::LessEqual);
+			case BuiltinFunction::GreaterEqual:
+				return new ComparisonGenerator<B, T>(this->m_builder, ComparisonOperator::GreaterEqual);
+			case BuiltinFunction::Equal:
+				return new ComparisonGenerator<B, T>(this->m_builder, ComparisonOperator::Equal);
+			case BuiltinFunction::NotEqual:
+				return new ComparisonGenerator<B, T>(this->m_builder, ComparisonOperator::NotEqual);
+			case BuiltinFunction::Plus:
+				return new BinaryGenerator<B, T>(this->m_builder, BinaryOperation::Plus);
+			case BuiltinFunction::Minus:
+				return new BinaryGenerator<B, T>(this->m_builder, BinaryOperation::Minus);
+			case BuiltinFunction::Multiply:
+				return new BinaryGenerator<B, T>(this->m_builder, BinaryOperation::Multiply);
+			case BuiltinFunction::Divide:
+				return new BinaryGenerator<B, T>(this->m_builder, BinaryOperation::Divide);
+			case BuiltinFunction::Power:
+				return new ExternalBinaryGenerator<B, T>(this->m_builder, ExternalBinaryOperation::Power);
+			case BuiltinFunction::Logarithm2:
+				return new ExternalBinaryGenerator<B, T>(this->m_builder, ExternalBinaryOperation::Logarithm);
+			case BuiltinFunction::Modulo:
+				return new ExternalBinaryGenerator<B, T>(this->m_builder, ExternalBinaryOperation::Modulo);
+			case BuiltinFunction::And:
+				return new BinaryGenerator<B, T>(this->m_builder, BinaryOperation::And);
+			case BuiltinFunction::Or:
+				return new BinaryGenerator<B, T>(this->m_builder, BinaryOperation::Or);
+			case BuiltinFunction::Nand:
+				return new BinaryGenerator<B, T>(this->m_builder, BinaryOperation::Nand);
+			case BuiltinFunction::Nor:
+				return new BinaryGenerator<B, T>(this->m_builder, BinaryOperation::Nor);
+			case BuiltinFunction::Xor:
+				return new BinaryGenerator<B, T>(this->m_builder, BinaryOperation::Xor);
+			case BuiltinFunction::Compress:
+				return new CompressionGenerator<B, T>(this->m_builder);
+			case BuiltinFunction::Count:
+				return new ReductionGenerator<B, T>(this->m_builder, ReductionOperation::Count);
+			case BuiltinFunction::Sum:
+				return new ReductionGenerator<B, T>(this->m_builder, ReductionOperation::Sum);
+			case BuiltinFunction::Average:
+				return new ReductionGenerator<B, T>(this->m_builder, ReductionOperation::Average);
+			case BuiltinFunction::Minimum:
+				return new ReductionGenerator<B, T>(this->m_builder, ReductionOperation::Minimum);
+			case BuiltinFunction::Maximum:
+				return new ReductionGenerator<B, T>(this->m_builder, ReductionOperation::Maximum);
+			case BuiltinFunction::Fill:
+				return new FillGenerator<B, T>(this->m_builder);
+			case BuiltinFunction::Unsupported:
+				return nullptr;
 		}
-		else if (name == "@neg")
-		{
-			return new UnaryGenerator<B, T>(this->m_builder, UnaryOperation::Negate);
-		}
-		else if (name == "@ceil")
-		{
-			return new RoundingGenerator<B, T>(this->m_builder, RoundingOperation::Ceiling);
-		}
-		else if (name == "@floor")
-		{
-			return new RoundingGenerator<B, T>(this->m_builder, RoundingOperation::Floor);
-		}
-		else if (name == "@round")
-		{
-			return new RoundingGenerator<B, T>(this->m_builder, RoundingOperation::Nearest);
-		}
-		else if (name == "@conj")
-		{
-			//TODO: Add support for complex numbers
-			std::cerr << "[ERROR] Complex number functions are not supported" << std::endl;
-			std::exit(EXIT_FAILURE);
-		}
-		else if (name == "@recip")
-		{
-			return new UnaryGenerator<B, T>(this->m_builder, UnaryOperation::Reciprocal);
-		}
-		else if (name == "@signum")
-		{
-			return new ComparisonGenerator<B, T>(this->m_builder, ComparisonOperator::Sign);
-		}
-		else if (name == "@pi")
-		{
-			return new UnaryGenerator<B, T>(this->m_builder, UnaryOperation::Pi);
-		}
-		else if (name == "@not")
-		{
-			return new UnaryGenerator<B, T>(this->m_builder, UnaryOperation::Not);
-		}
-		else if (name == "@log")
-		{
-			return new ExternalUnaryGenerator<B, T>(this->m_builder, ExternalUnaryOperation::Logarithm);
-		}
-		else if (name == "@exp")
-		{
-			return new ExternalUnaryGenerator<B, T>(this->m_builder, ExternalUnaryOperation::Exponential);
-		}
-		else if (name == "@cos")
-		{
-			return new ExternalUnaryGenerator<B, T>(this->m_builder, ExternalUnaryOperation::Cosine);
-		}
-		else if (name == "@sin")
-		{
-			return new ExternalUnaryGenerator<B, T>(this->m_builder, ExternalUnaryOperation::Sine);
-		}
-		else if (name == "@tan")
-		{
-			return new ExternalUnaryGenerator<B, T>(this->m_builder, ExternalUnaryOperation::Tangent);
-		}
-		else if (name == "@acos")
-		{
-			return new ExternalUnaryGenerator<B, T>(this->m_builder, ExternalUnaryOperation::InverseCosine);
-		}
-		else if (name == "@asin")
-		{
-			return new ExternalUnaryGenerator<B, T>(this->m_builder, ExternalUnaryOperation::InverseSine);
-		}
-		else if (name == "@atan")
-		{
-			return new ExternalUnaryGenerator<B, T>(this->m_builder, ExternalUnaryOperation::Tangent);
-		}
-		else if (name == "@cosh")
-		{
-			return new ExternalUnaryGenerator<B, T>(this->m_builder, ExternalUnaryOperation::HyperbolicCosine);
-		}
-		else if (name == "@sinh")
-		{
-			return new ExternalUnaryGenerator<B, T>(this->m_builder, ExternalUnaryOperation::HyperbolicSine);
-		}
-		else if (name == "@tanh")
-		{
-			return new ExternalUnaryGenerator<B, T>(this->m_builder, ExternalUnaryOperation::HyperbolicTangent);
-		}
-		else if (name == "@acosh")
-		{
-			return new ExternalUnaryGenerator<B, T>(this->m_builder, ExternalUnaryOperation::HyperbolicInverseCosine);
-		}
-		else if (name == "@asinh")
-		{
-			return new ExternalUnaryGenerator<B, T>(this->m_builder, ExternalUnaryOperation::HyperbolicInverseSine);
-		}
-		else if (name == "@atanh")
-		{
-			return new ExternalUnaryGenerator<B, T>(this->m_builder, ExternalUnaryOperation::HyperbolicInverseTangent);
-		}
-		else if (name == "@lt")
-		{
-			return new ComparisonGenerator<B, T>(this->m_builder, ComparisonOperator::Less);
-		}
-		else if (name == "@gt")
-		{
-			return new ComparisonGenerator<B, T>(this->m_builder, ComparisonOperator::Greater);
-		}
-		else if (name == "@leq")
-		{
-			return new ComparisonGenerator<B, T>(this->m_builder, ComparisonOperator::LessEqual);
-		}
-		else if (name == "@geq")
-		{
-			return new ComparisonGenerator<B, T>(this->m_builder, ComparisonOperator::GreaterEqual);
-		}
-		else if (name == "@eq")
-		{
-			return new ComparisonGenerator<B, T>(this->m_builder, ComparisonOperator::Equal);
-		}
-		else if (name == "@neq")
-		{
-			return new ComparisonGenerator<B, T>(this->m_builder, ComparisonOperator::NotEqual);
-		}
-		else if (name == "@plus")
-		{
-			return new BinaryGenerator<B, T>(this->m_builder, BinaryOperation::Plus);
-		}
-		else if (name == "@minus")
-		{
-			return new BinaryGenerator<B, T>(this->m_builder, BinaryOperation::Minus);
-		}
-		else if (name == "@mul")
-		{
-			return new BinaryGenerator<B, T>(this->m_builder, BinaryOperation::Multiply);
-		}
-		else if (name == "@div")
-		{
-			return new BinaryGenerator<B, T>(this->m_builder, BinaryOperation::Divide);
-		}
-		else if (name == "@power")
-		{
-			return new ExternalBinaryGenerator<B, T>(this->m_builder, ExternalBinaryOperation::Power);
-		}
-		else if (name == "@log2")
-		{
-			return new ExternalBinaryGenerator<B, T>(this->m_builder, ExternalBinaryOperation::Logarithm);
-		}
-		else if (name == "@mod")
-		{
-			return new ExternalBinaryGenerator<B, T>(this->m_builder, ExternalBinaryOperation::Modulo);
-		}
-		else if (name == "@and")
-		{
-			return new BinaryGenerator<B, T>(this->m_builder, BinaryOperation::And);
-		}
-		else if (name == "@or")
-		{
-			return new BinaryGenerator<B, T>(this->m_builder, BinaryOperation::Or);
-		}
-		else if (name == "@nand")
-		{
-			return new BinaryGenerator<B, T>(this->m_builder, BinaryOperation::Nand);
-		}
-		else if (name == "@nor")
-		{
-			return new BinaryGenerator<B, T>(this->m_builder, BinaryOperation::Nor);
-		}
-		else if (name == "@xor")
-		{
-			return new BinaryGenerator<B, T>(this->m_builder, BinaryOperation::Xor);
-		}
-		else if (name == "@compress")
-		{
-			return new CompressionGenerator<B, T>(this->m_builder);
-		}
-		else if (name == "@count")
-		{
-			return new ReductionGenerator<B, T>(this->m_builder, ReductionOperation::Count);
-		}
-		else if (name == "@sum")
-		{
-			return new ReductionGenerator<B, T>(this->m_builder, ReductionOperation::Sum);
-		}
-		else if (name == "@avg")
-		{
-			return new ReductionGenerator<B, T>(this->m_builder, ReductionOperation::Average);
-		}
-		else if (name == "@min")
-		{
-			return new ReductionGenerator<B, T>(this->m_builder, ReductionOperation::Minimum);
-		}
-		else if (name == "@max")
-		{
-			return new ReductionGenerator<B, T>(this->m_builder, ReductionOperation::Maximum);
-		}
-		else if (name == "@fill")
-		{
-			return new FillGenerator<B, T>(this->m_builder);
-		}
-		return nullptr;
 	}
 
 protected:
