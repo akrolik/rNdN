2426b5738be9d352c081705581e00751fc8cc2b2
Alexander Krolik <alexander.krolik@mail.mcgill.ca>
2019-06-20 11:34:57 -0400

    Add indentation to analysis printer.

--- include/HorseIR/Analysis/FlowAnalysisPrinter.h
+++ include/HorseIR/Analysis/FlowAnalysisPrinter.h
@@ -22,26 +22,50 @@ public:
 
 	FlowAnalysisPrinter(const FlowAnalysis<F>& analysis) : m_analysis(analysis) {}
 
+	std::string Indent() const
+	{
+		std::string str;
+		for (unsigned int i = 0; i < m_indent; ++i)
+		{
+			str += "\t";
+		}
+		return str;
+	}
+
 	bool VisitIn(const Statement *statement) override
 	{
-		m_string << "------------------------------------------------" << std::endl;
-		m_string << PrettyPrinter::PrettyString(statement);
-		m_string << "------------------------------------------------" << std::endl;
+		std::string indent = Indent();
 
-	        m_string << "\tIn: {" << std::endl;
-		m_analysis.GetInSet(statement).Print(m_string, 2);
-		m_string << std::endl << "\t}" << std::endl;
+		m_string << indent << "------------------------------------------------" << std::endl;
+		m_string << indent << PrettyPrinter::PrettyString(statement);
+		m_string << indent << "------------------------------------------------" << std::endl;
 
-	        m_string << "\tOut: {" << std::endl;
-		m_analysis.GetOutSet(statement).Print(m_string, 2);
-		m_string << std::endl << "\t}" << std::endl;
+		m_indent++;
+		indent = Indent();
+
+	        m_string << indent << "In: {" << std::endl;
+		m_analysis.GetInSet(statement).Print(m_string, m_indent + 1);
+		m_string << std::endl;
+		m_string << indent << "}" << std::endl;
+
+	        m_string << indent << "Out: {" << std::endl;
+		m_analysis.GetOutSet(statement).Print(m_string, m_indent + 1);
+		m_string << std::endl;
+		m_string << indent << "}" << std::endl;
 		
+		m_indent--;
 		return false;
 	}
 
 	bool VisitIn(const BlockStatement *blockS) override
 	{
+		m_indent++;
 		return true;
+	}
+
+	void VisitOut(const BlockStatement *blockS) override
+	{
+		m_indent--;
 	}
 
 	bool VisitIn(const IfStatement *ifS) override
@@ -64,6 +88,7 @@ public:
 
 protected:
 	std::stringstream m_string;
+	unsigned int m_indent = 0;
 
 	const FlowAnalysis<F>& m_analysis;
 };
