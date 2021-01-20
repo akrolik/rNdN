c5dcf95d278d52d4ae5cc1f1188e6c1c50e76c59
Alexander Krolik <alexander.krolik@mail.mcgill.ca>
2021-01-20 18:31:09 -0500

    Move register allocation implementation to object file.

--- include/PTX/Analysis/RegisterAllocator/RegisterAllocation.h
+++ include/PTX/Analysis/RegisterAllocator/RegisterAllocation.h
@@ -16,103 +16,24 @@ public:
 
 	// Registers
 
-	void AddRegister(const std::string& name, std::uint8_t reg, std::uint8_t range = 1)
-	{
-		m_registerMap[name] = {reg, range};
-
-		auto maxRegister = reg + (range - 1);
-		if (maxRegister > m_registerCount)
-		{
-			m_registerCount = maxRegister;
-		}
-	}
-
-	bool ContainsRegister(const std::string& name)
-	{
-		return m_registerMap.find(name) != m_registerMap.end();
-	}
-
-	const std::pair<std::uint8_t, std::uint8_t>& GetRegister(const std::string& name)
-	{
-		return m_registerMap.at(name);
-	}
+	void AddRegister(const std::string& name, std::uint8_t reg, std::uint8_t range = 1);
+	bool ContainsRegister(const std::string& name) const;
+	const std::pair<std::uint8_t, std::uint8_t>& GetRegister(const std::string& name) const;
 
 	// Predicate
 
-	void AddPredicate(const std::string& name, std::uint8_t reg)
-	{
-		m_predicateMap[name] = reg;
-
-		auto maxPredicate = reg + 1;
-		if (maxPredicate > m_predicateCount)
-		{
-			m_predicateCount = maxPredicate;
-		}
-	}
-
-	bool ContainsPredicate(const std::string& name)
-	{
-		return m_predicateMap.find(name) != m_predicateMap.end();
-	}
-
-	std::uint8_t GetPredicate(const std::string& name)
-	{
-		return m_predicateMap.at(name);
-	}
+	void AddPredicate(const std::string& name, std::uint8_t reg);
+	bool ContainsPredicate(const std::string& name) const;
+	std::uint8_t GetPredicate(const std::string& name) const;
 
 	// Counts
 
-	std::uint8_t GetRegisterCount() const
-	{
-		return m_registerCount;
-	}
-
-	std::uint8_t GetPredicateCount() const
-	{
-		return m_predicateCount;
-	}
+	std::uint8_t GetRegisterCount() const { return m_registerCount; }
+	std::uint8_t GetPredicateCount() const { return m_predicateCount; }
 
 	// Formatting
 
-	std::string ToString() const
-	{
-		std::string string = "  - Registers = " + std::to_string(m_registerCount) + " + [R0 dummy]";
-		for (const auto& [name, pair] : m_registerMap)
-		{
-			string += "\n    - " + name + "->";
-
-			auto reg = pair.first;
-			auto range = pair.second;
-
-			if (range > 1)
-			{
-				string += "{";
-			}
-
-			auto first = true;
-			for (auto i = 0u; i < range; ++i)
-			{
-				if (!first)
-				{
-					string += ", ";
-				}
-				first = false;
-				string += "R" + std::to_string(reg + i);
-			}
-
-			if (range > 1)
-			{
-				string += "}";
-			}
-		}
-
-		string += "\n  - Predicates = " + std::to_string(m_predicateCount);
-		for (const auto& [name, reg] : m_predicateMap)
-		{
-			string += "\n    - " + name + "->P" + std::to_string(reg);
-		}
-		return string;
-	}
+	std::string ToString() const;
 
 private:
 	std::unordered_map<std::string, std::pair<std::uint8_t, std::uint8_t>> m_registerMap;
---
+++ src/PTX/Analysis/RegisterAllocator/RegisterAllocation.cpp
@@ -0,0 +1,95 @@
+#include "PTX/Analysis/RegisterAllocator/RegisterAllocation.h"
+
+namespace PTX {
+namespace Analysis {
+
+// Registers
+
+void RegisterAllocation::AddRegister(const std::string& name, std::uint8_t reg, std::uint8_t range)
+{
+	m_registerMap[name] = {reg, range};
+
+	auto maxRegister = reg + range;
+	if (maxRegister > m_registerCount)
+	{
+		m_registerCount = maxRegister;
+	}
+}
+
+bool RegisterAllocation::ContainsRegister(const std::string& name) const
+{
+	return m_registerMap.find(name) != m_registerMap.end();
+}
+
+const std::pair<std::uint8_t, std::uint8_t>& RegisterAllocation::GetRegister(const std::string& name) const
+{
+	return m_registerMap.at(name);
+}
+
+// Predicate
+
+void RegisterAllocation::AddPredicate(const std::string& name, std::uint8_t reg)
+{
+	m_predicateMap[name] = reg;
+
+	auto maxPredicate = reg + 1;
+	if (maxPredicate > m_predicateCount)
+	{
+		m_predicateCount = maxPredicate;
+	}
+}
+
+bool RegisterAllocation::ContainsPredicate(const std::string& name) const
+{
+	return m_predicateMap.find(name) != m_predicateMap.end();
+}
+
+std::uint8_t RegisterAllocation::GetPredicate(const std::string& name) const
+{
+	return m_predicateMap.at(name);
+}
+
+// Formatting
+
+std::string RegisterAllocation::ToString() const
+{
+	std::string string = "  - Registers = " + std::to_string(m_registerCount) + " (R0 dummy)";
+	for (const auto& [name, pair] : m_registerMap)
+	{
+		string += "\n    - " + name + "->";
+
+		auto reg = pair.first;
+		auto range = pair.second;
+
+		if (range > 1)
+		{
+			string += "{";
+		}
+
+		auto first = true;
+		for (auto i = 0u; i < range; ++i)
+		{
+			if (!first)
+			{
+				string += ", ";
+			}
+			first = false;
+			string += "R" + std::to_string(reg + i);
+		}
+
+		if (range > 1)
+		{
+			string += "}";
+		}
+	}
+
+	string += "\n  - Predicates = " + std::to_string(m_predicateCount);
+	for (const auto& [name, reg] : m_predicateMap)
+	{
+		string += "\n    - " + name + "->P" + std::to_string(reg);
+	}
+	return string;
+}
+
+}
+}
