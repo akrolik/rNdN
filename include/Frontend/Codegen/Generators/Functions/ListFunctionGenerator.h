89766285b1cdbe106d92f84c4fc734f36941361d
Alexander Krolik <alexander.krolik@mail.mcgill.ca>
2021-03-31 20:09:59 -0400

    Standardize list loop exit.

--- include/Frontend/Codegen/Generators/Functions/ListFunctionGenerator.h
+++ include/Frontend/Codegen/Generators/Functions/ListFunctionGenerator.h
@@ -86,88 +86,92 @@ public:
 		ThreadGeometryGenerator<B> geometryGenerator(this->m_builder);
 		auto listSize = geometryGenerator.GenerateListGeometry();
 
-		auto exitPredicate = resources->template AllocateTemporary<PTX::PredicateType>();
-		this->m_builder.AddStatement(new PTX::SetPredicateInstruction<PTX::UInt32Type>(exitPredicate, listIndex, listSize, PTX::UInt32Type::ComparisonOperator::GreaterEqual));
-
-		auto exitInstruction = new PTX::ReturnInstruction();
-		exitInstruction->SetPredicate(exitPredicate);
-		this->m_builder.AddStatement(exitInstruction);
-
-		// Load the geometry cell sizes
-
-		this->m_builder.AddStatement(new PTX::CommentStatement(NameUtils::ThreadGeometryDataSize));
-
-		auto geometryDataSizeParameter = parameterGenerator.template GeneratePointer<PTX::UInt32Type>(NameUtils::ThreadGeometryDataSize);
-		parameterLoadGenerator.template GenerateParameterAddress<PTX::UInt32Type>(geometryDataSizeParameter);
-
-		ValueLoadGenerator<B, PTX::UInt32Type> valueLoadGenerator(this->m_builder);
-		valueLoadGenerator.GeneratePointer(geometryDataSizeParameter, listIndex);
-
-		// Initialize the special local index register
-
-		this->m_builder.AddStatement(new PTX::CommentStatement(NameUtils::ThreadGeometryListDataIndex));
-
-		auto index = indexGenerator.GenerateListDataIndex();
-
-		// Fetch the arguments from the .param address space
-
-		parameterLoadGenerator.Generate(function);
-
-		// Generate the starting index for the loop
-
-		this->m_builder.AddStatement(new PTX::CommentStatement("loop init"));
-
-		// Generate the loop bound
-
-		auto listDataSize = geometryGenerator.GenerateListDataGeometry();
-		auto bound = resources->template AllocateTemporary<PTX::UInt32Type>();
-		this->m_builder.AddStatement(new PTX::AddInstruction<PTX::UInt32Type>(bound, index, listDataSize));
-
-		// Construct the standard loop structure
-		//
-		//   <init>
-		//   setp %p1, ...
-		//   @%p1 br END
-		//
-		//   START:
-		//      <body>
-		//      <post>
-		//
-		//      setp %p2, ...
-		//      @%p2 br START
-		//
-		//   END:
-
-		this->m_builder.AddIfStatement("LIST_SKIP", [&]()
+		this->m_builder.AddIfStatement("LIST_BOUND", [&]()
 		{
-			auto predicate = resources->template AllocateTemporary<PTX::PredicateType>();
+			auto exitPredicate = resources->template AllocateTemporary<PTX::PredicateType>();
 			this->m_builder.AddStatement(new PTX::SetPredicateInstruction<PTX::UInt32Type>(
-				predicate, index, bound, PTX::UInt32Type::ComparisonOperator::GreaterEqual
+				exitPredicate, listIndex, listSize, PTX::UInt32Type::ComparisonOperator::GreaterEqual
 			));
-			return std::make_tuple(predicate, false);
+			return std::make_tuple(exitPredicate, false);
 		},
 		[&]()
 		{
-			this->m_builder.AddDoWhileLoop("LIST", [&](Builder::LoopContext& loopContext)
+			// Load the geometry cell sizes
+
+			this->m_builder.AddStatement(new PTX::CommentStatement(NameUtils::ThreadGeometryDataSize));
+
+			auto geometryDataSizeParameter = parameterGenerator.template GeneratePointer<PTX::UInt32Type>(NameUtils::ThreadGeometryDataSize);
+			parameterLoadGenerator.template GenerateParameterAddress<PTX::UInt32Type>(geometryDataSizeParameter);
+
+			ValueLoadGenerator<B, PTX::UInt32Type> valueLoadGenerator(this->m_builder);
+			valueLoadGenerator.GeneratePointer(geometryDataSizeParameter, listIndex);
+
+			// Initialize the special local index register
+
+			this->m_builder.AddStatement(new PTX::CommentStatement(NameUtils::ThreadGeometryListDataIndex));
+
+			auto index = indexGenerator.GenerateListDataIndex();
+
+			// Fetch the arguments from the .param address space
+
+			parameterLoadGenerator.Generate(function);
+
+			// Generate the starting index for the loop
+
+			this->m_builder.AddStatement(new PTX::CommentStatement("loop init"));
+
+			// Generate the loop bound
+
+			auto listDataSize = geometryGenerator.GenerateListDataGeometry();
+			auto bound = resources->template AllocateTemporary<PTX::UInt32Type>();
+			this->m_builder.AddStatement(new PTX::AddInstruction<PTX::UInt32Type>(bound, index, listDataSize));
+
+			// Construct the standard loop structure
+			//
+			//   <init>
+			//   setp %p1, ...
+			//   @%p1 br END
+			//
+			//   START:
+			//      <body>
+			//      <post>
+			//
+			//      setp %p2, ...
+			//      @%p2 br START
+			//
+			//   END:
+
+			this->m_builder.AddIfStatement("LIST_SKIP", [&]()
 			{
-				// Construct the loop body according to the standard function generator
-
-				FunctionGenerator<B>::Visit(function);
-
-				// Increment the thread index by the number of threads per cell
-
-				this->m_builder.AddStatement(new PTX::CommentStatement("loop post"));
-
-				auto listThreads = geometryGenerator.GenerateListThreads();
-				this->m_builder.AddStatement(new PTX::AddInstruction<PTX::UInt32Type>(index, index, listThreads));
-
-				// Complete the loop structure
-
-				auto predicateEnd = resources->template AllocateTemporary<PTX::PredicateType>();
+				auto predicate = resources->template AllocateTemporary<PTX::PredicateType>();
 				this->m_builder.AddStatement(new PTX::SetPredicateInstruction<PTX::UInt32Type>(
-					predicateEnd, index, bound, PTX::UInt32Type::ComparisonOperator::Less
+					predicate, index, bound, PTX::UInt32Type::ComparisonOperator::GreaterEqual
 				));
-				return std::make_tuple(predicateEnd, false);
+				return std::make_tuple(predicate, false);
+			},
+			[&]()
+			{
+				this->m_builder.AddDoWhileLoop("LIST", [&](Builder::LoopContext& loopContext)
+				{
+					// Construct the loop body according to the standard function generator
+
+					FunctionGenerator<B>::Visit(function);
+
+					// Increment the thread index by the number of threads per cell
+
+					this->m_builder.AddStatement(new PTX::CommentStatement("loop post"));
+
+					auto listThreads = geometryGenerator.GenerateListThreads();
+					this->m_builder.AddStatement(new PTX::AddInstruction<PTX::UInt32Type>(index, index, listThreads));
+
+					// Complete the loop structure
+
+					auto predicateEnd = resources->template AllocateTemporary<PTX::PredicateType>();
+					this->m_builder.AddStatement(new PTX::SetPredicateInstruction<PTX::UInt32Type>(
+						predicateEnd, index, bound, PTX::UInt32Type::ComparisonOperator::Less
+					));
+					return std::make_tuple(predicateEnd, false);
+				});
 			});
 		});
 
