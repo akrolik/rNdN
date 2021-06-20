f1eabb31789a0e8cb931394a2622bc70858fd910
Alexander Krolik <alexander.krolik@mail.mcgill.ca>
2021-06-20 18:42:03 -0400

    Update run script.

--- results/run_ptxas.sh
+++ results/run_ptxas.sh
@@ -9,7 +9,7 @@ do
 	echo "Query $i"
 	echo "===================================="
 
-	../build/bin/r3d3 --debug-time --backend=ptxas --data-load-tpch --data-path-tpch="$DATA_PATH" "../tests/tpch/q${i}.hir" | tee "q${i}_ptxas.log"
+	../build/bin/r3d3 --debug-time --backend=ptxas --data-load-tpch="$DATA_PATH" "../tests/tpch/q${i}.hir" | tee "q${i}_ptxas.log"
 
 	echo 
 done
