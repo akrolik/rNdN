eb2b9523f9cfb4755531469bbd7209bc41e8c574
Alexander Krolik <alexander.krolik@mail.mcgill.ca>
2021-07-07 16:55:10 -0400

    Add extra move types.

--- include/Frontend/Codegen/Generators/Expressions/MoveGenerator.h
+++ include/Frontend/Codegen/Generators/Expressions/MoveGenerator.h
@@ -19,16 +19,29 @@ public:
 
 	void Generate(PTX::Register<T> *destination, PTX::TypedOperand<T> *value)
 	{
-		if constexpr(std::is_same<T, PTX::Int8Type>::value)
+		auto resources = this->m_builder.GetLocalResources();
+		if constexpr(std::is_same<T, PTX::Bit8Type>::value)
 		{
-			auto resources = this->m_builder.GetLocalResources();
+			auto bracedSource = new PTX::Braced2Operand<PTX::Bit8Type>({
+				value, new PTX::Value<PTX::Bit8Type>(0)
+			});
+			auto bracedTarget = new PTX::Braced2Register<PTX::Bit8Type>({
+				destination, new PTX::SinkRegister<PTX::Bit8Type>()
+			});
+			auto temp = resources->template AllocateTemporary<PTX::Bit16Type>();
+
+			this->m_builder.AddStatement(new PTX::Pack2Instruction<PTX::Bit16Type>(temp, bracedSource));
+			this->m_builder.AddStatement(new PTX::Unpack2Instruction<PTX::Bit16Type>(bracedTarget, temp));
+		}
+		else if constexpr(std::is_same<T, PTX::Int8Type>::value)
+		{
 			auto bracedSource = new PTX::Braced2Operand<PTX::Bit8Type>({
 				new PTX::Bit8Adapter<PTX::IntType>(value),
 				new PTX::Value<PTX::Bit8Type>(0)
 			});
 			auto bracedTarget = new PTX::Braced2Register<PTX::Bit8Type>({
 				new PTX::Bit8RegisterAdapter<PTX::IntType>(destination),
-				new PTX::SinkRegister<PTX::Bit8Type>
+				new PTX::SinkRegister<PTX::Bit8Type>()
 			});
 			auto temp = resources->template AllocateTemporary<PTX::Bit16Type>();
 
