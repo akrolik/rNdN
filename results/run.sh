9eaed74723d75d93d5e3ac6a77168ea2b2271753
Alexander Krolik <alexander.krolik@mail.mcgill.ca>
2022-01-12 23:03:34 -0500

    Update run script logging.

--- results/run.sh
+++ results/run.sh
@@ -4,5 +4,8 @@ make -j -C ../build
 
 for i in {1..22}
 do
+	echo "Query $i"
+	echo "===================================="
+
 	./run_single.sh $i "r3d3" ""
 done
--- results/run_single.sh
+++ results/run_single.sh
@@ -4,15 +4,13 @@ DATA_PATH="/mnt/local/alex/data/tpc-h/"
 OUTPUT_PATH="q${1}_${2}.log"
 OPTIONS="$3"
 
-echo "Query $1 (Options=$OPTIONS)"
-echo "===================================="
+../build/bin/r3d3 $OPTIONS --debug-time --debug-options --data-load-tpch="$DATA_PATH" "../tests/tpch/q${1}.hir" | tee "$OUTPUT_PATH"
 
-../build/bin/r3d3 $OPTIONS --optimize-sass --debug-time --data-load-tpch="$DATA_PATH" "../tests/tpch/q${1}.hir" | tee "$OUTPUT_PATH"
-
-RESULT=$(cat "$OUTPUT_PATH" | grep -v "\[" | grep -v "^Loading" | grep -v "^Execut" | grep -v "^Init" | grep -v "^Parsing"| grep -v "^\$")
+RESULT=$(cat "$OUTPUT_PATH" | grep -v "\[" | grep -v "^Debug" | grep -v "^Loading" | grep -v "^Execut" | grep -v "^Init" | grep -v "^Parsing"| grep -v "^\$")
 EXPECTED=$(cat "expected/q${1}.log")
 
 # Query 1 has double precision ouput that varies between runs, remove the decimal
+
 if [[ "$1" == "1" ]]; then
 	RESULT=$(echo "$RESULT" | sed 's/\([0-9]\{10,\}\)\.[0-9]*/\1.000000/g')
 	EXPECTED=$(echo "$EXPECTED" | sed 's/\([0-9]\{10,\}\)\.[0-9]*/\1.000000/g')
